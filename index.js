7e4fcdbc8b6b70acdee46ca326ea38e6bb4c7428
Agnes <agsawicka@gmail.com>
2022-07-10 22:17:11 -0400

    commit tree 9de5fb9297f2ac8defd68b1ca2a71668b3d0d4e2

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+if (word.length === 1){
+  return true
 }
+if (word.length ===2){
+  return word[0]===word[1]
+}
+
+let first = word[0]
+let last  = word.slice(-1)
+
+if (first !== last){
+  return false
+}
+ let result = isPalindrome(word.slice(1, -1))
+ return result
+}
+
+
 
 /* 
   Add your pseudocode here
+function takes a string, 
+slice one and last element of the string and compare it,
+if first and last is the same takes again last and first
+if diffrent return false, 
+at the end if 1 letter left return true
+last === first
+last === first
+length === 1
+palindrome
+
+
+
+
 */
 
 /*
